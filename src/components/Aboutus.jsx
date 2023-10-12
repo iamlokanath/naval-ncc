39167c7fcd12551ebf38c578bf3744bf22a7e0dc
Lkanath Panda <lokanthpanda128@gmai.com>
2023-10-12 16:16:01 +0530

    aboutus, activities added

--- src/components/Aboutus.jsx
+++ src/components/Aboutus.jsx
@@ -1,15 +1,71 @@
-import React from 'react'
-import Navbar from './Navbar'
-
+import React from "react";
+import Navbar from "./Navbar";
+import "./Aboutus.css";
+// import About1 from "../Image/Football.jpeg"
+import About2 from "../Image/Gcek1.jpeg";
 const Aboutus = () => {
   return (
     <>
-    <Navbar/>
-    <div>
-      About US coming soon
-    </div>
-    </>
-  )
-}
+      <Navbar />
 
-export default Aboutus
+      <div className="gcekabout">
+          <div>
+            <h2>
+              About <span>Us</span>
+            </h2>
+            <h4>GCEK Naval NCC, Bhawanipatna </h4>
+          </div>
+
+          <div className="gcekaboutcontainer">
+            <div className="container">
+              <div className="row m-b-n30 align-items-center">
+                <div
+                  className="col-lg-6 m-b-30 aos-init aos-animate"
+                  data-aos="fade-up"
+                  data-aos-duration="1100"
+                >
+                  <div className="about-content mw-100">
+                    <h2 className="title">
+                    GCEK Naval NCC:<br/> Shaping Future Leaders through Dedication, Discipline, and Service to the Nation.
+                    </h2>
+                    <span></span>
+                    <p>
+                      GCEK Naval NCC in Bhawanipatna is a prestigious
+                      organization that instills the values of discipline,
+                      courage, and leadership among the youth. It provides ample
+                      opportunities to the students to participate in various
+                      activities such as drill, sailing, shooting, and adventure
+                      sports. With its comprehensive training and guidance, GCEK
+                      Naval NCC prepares the students for a successful career in
+                      the armed forces and other sectors.
+
+                     
+
+                      
+                      
+                    </p>
+                  </div>
+                </div>
+                <div
+                  className="col-lg-6 order-first order-lg-last m-b-30 aos-init aos-animate"
+                  data-aos="fade-up"
+                  data-aos-duration="1500"
+                >
+                  <div className="about-image service-layer-image ms-auto">
+                    <img
+                      className="about-image-1 img-fluid"
+                      src={About2}
+                      alt="About"
+                    />
+                    {/* <img className="about-image-2 img-fluid" src={About2} alt="About" /> */}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </>
+  );
+};
+
+export default Aboutus;
--- src/components/Activities.jsx
+++ src/components/Activities.jsx
@@ -6,8 +6,310 @@ const Activities = () => {
     <>
     <Navbar/>
     
-    <div>Activities Comming soon,<br /> Stay Updated to Discover The Thrill Of Adventure With Our Exciting Activities</div>
+    <div className=" container gcekactivities">
+          <div>
+            <h2>
+              Our <span>Activities</span>
+            </h2>
+            <h4>Discover The Thrill Of Adventure With Our Exciting Activities</h4>
+          </div>
 
+
+{/* 
+          <div className="acitivitiestable">
+                <div className="acitivitiestablecontainer">
+
+
+                    <div className="activitiesrow1">
+                    <div className="acitivities1">
+                        <div className="acitivities1container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Theory Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="acitivities2">
+                        <div className="acitivities2container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Practical Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="acitivities3">
+                        <div className="acitivities3container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Note Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+                    
+
+                    <div className="activitiesrow1">
+                    <div className="acitivities1">
+                        <div className="acitivities1container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Theory Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="acitivities2">
+                        <div className="acitivities2container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Practical Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="acitivities3">
+                        <div className="acitivities3container">
+                            <div className="activitiesicon">
+                                <i className="fas fa-drum"></i>
+                            </div>
+                            <div className="activitiesinfo">
+                                <h4>Note Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    </div>
+
+
+                    
+                    </div>
+            </div> */}
+
+
+
+<div className="row features-items gcekactivitieslist">
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-tv"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Theory Class</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-water"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Drill</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-mountain"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Mountain Trekking</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <hr /> */}
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-ship"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Boat Pooling</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-volleyball-ball"></i>
+                            </div>
+                            <div className="info">
+                                <h4>SPORTS</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                            <i className="fas fa-fire"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Firing</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <hr /> */}
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-campground"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Camps</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-ship"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Ship Modelling</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-globe"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Cultural Events</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <hr /> */}
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-swimmer"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Swimming</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-flag"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Semaphore</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    
+
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-water"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Kayaking</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <hr /> */}
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-water"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Social Awareness</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-tree"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Tree Plantation</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-6 equal-height activitiesitems">
+                        <div className="item">
+                            <div className="icon">
+                                <i className="fas fa-dumbbell"></i>
+                            </div>
+                            <div className="info">
+                                <h4>Yoga</h4>
+                                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* <hr /> */}
+                   
+            </div>
+
+
+    </div>
     </>
   )
 }
